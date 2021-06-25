b5aaca2cf06819629c57067bf112d7e2e034fdfb
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-06-25 21:11:06 +0530

    try - 1

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, jsonify
-import gunicorn
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello_world')
-
 def hello_world():
-	return "Hello World"
+    return 'Hello World'
+
+@app.route('/api',methods=['GET'])
+def api():
+    d = {}
+    d['Query'] = str(request.args['Query'])
+    flag=int(d['Query'])
+    if flag==1:
+        f = open("file.txt", "w")
+        f.write("1")
+        f.close()
+    else:
+        f = open("file.txt", "w")
+        f.write("0")
+        f.close()
+    return jsonify(d)
 
 @app.route('/ans')
-
 def ans():
     file = open("file.txt", "r")
     text = file.read()
@@ -19,4 +29,4 @@ def ans():
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
